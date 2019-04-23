cc939ed6aacb3b369e80dedf836c269bdd251f35
John Hattas <johnhattas@gmail.com>
2019-04-23 03:11:41 -0500

    add support for ori in alu

--- alu_control.vhd
+++ alu_control.vhd
@@ -25,20 +25,23 @@ begin
 -- implement alu control truth table from page 261 of Computer Organization and Design
 process(aluop, funccode)
 begin
-	if aluop = "00" then
-		aluctl <= "0010"; --add
-	elsif aluop(0) = '1' then
-		aluctl <= "0110"; --sub
-	elsif aluop(1) = '1' then
-		case funccode(3 downto 0) is
-			when "0000" => aluctl <= "0010"; --add
-			when "0010" => aluctl <= "0110"; --sub
-			when "0100" => aluctl <= "0000"; --and
-			when "0101" => aluctl <= "0001"; --or
-			when "1010" => aluctl <= "0111"; --slt
-			when others => aluctl <= "XXXX"; --shouldn't happen
-		end case;
-	end if;
+	case aluop is
+		when "00" =>
+			aluctl <= "0010"; --add
+		when "01" =>
+			aluctl <= "0110"; --sub
+		when "11" =>
+			aluctl <= "0001"; --or
+		when "10" =>
+			case funccode(3 downto 0) is
+				when "0000" => aluctl <= "0010"; --add
+				when "0010" => aluctl <= "0110"; --sub
+				when "0100" => aluctl <= "0000"; --and
+				when "0101" => aluctl <= "0001"; --or
+				when "1010" => aluctl <= "0111"; --slt
+				when others => aluctl <= "XXXX"; --shouldn't happen
+			end case;
+	end case;
 end process;
 
 
