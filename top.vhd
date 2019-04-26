72cc8b539af98bc279378aadab444d22a20e0ee8
John Hattas <johnhattas@gmail.com>
2019-04-25 21:52:12 -0500

    refactor top - organize and comment

--- top.vhd
+++ top.vhd
@@ -18,12 +18,22 @@ entity top is
 end top;
 
 architecture arch of top is
+
     -- program counter
     signal pc : std_logic_vector(31 downto 0);
-
-	-- control signals
+	signal pcplus4 : std_logic_vector(31 downto 0);
+	signal pcplus8 : std_logic_vector(31 downto 0);
+    
+    -- instruction fields
+	signal instruction : std_logic_vector(31 downto 0);
 	signal opcode : std_logic_vector(5 downto 0);
+	signal rs, rt, rd : std_logic_vector(4 downto 0);
+	signal shamt : std_logic_vector(4 downto 0);
 	signal funct : std_logic_vector(5 downto 0);
+	signal immediate : std_logic_vector(15 downto 0);
+	signal address : std_logic_vector(25 downto 0);
+	
+	-- control signals
 	signal regdst: std_logic_vector(1 downto 0);
 	signal jump: std_logic;
 	signal branch: std_logic;
@@ -37,69 +47,38 @@ architecture arch of top is
 	signal jumpreg: std_logic;
 	
 	-- register signals
-	signal read_reg1 : std_logic_vector(4 downto 0);
-	signal read_reg2 : std_logic_vector(4 downto 0);
 	signal write_reg : std_logic_vector(4 downto 0);
     signal write_data : std_logic_vector(31 downto 0);
     signal read_data1 : std_logic_vector(31 downto 0);
 	signal read_data2 : std_logic_vector(31 downto 0);
 	
-	-- branch and jump signals
-	signal pcplus4 : std_logic_vector(31 downto 0);
-	signal pcplus8 : std_logic_vector(31 downto 0);
-	
 	-- alu signals
-	signal zeroflag : std_logic;
 	signal alu_in_a : std_logic_vector(31 downto 0);
 	signal alu_in_b : std_logic_vector(31 downto 0);
 	signal alu_result : std_logic_vector(31 downto 0);
-	signal funccode : std_logic_vector(5 downto 0);
+	signal zeroflag : std_logic;
 	
 	-- data memory signals
 	signal dm_read_data : std_logic_vector(31 downto 0);
-	
-	-- instruction memory
-	signal instruction : std_logic_vector(31 downto 0);
-	
+		
 	-- intermediate misc. signals
-	signal immediate : std_logic_vector(15 downto 0);
-	signal shamt : std_logic_vector(4 downto 0);
 	signal branch_sel : std_logic;
-	signal branch_adder_result : std_logic_vector(31 downto 0);
-	signal jump_mux_output : std_logic_vector(31 downto 0);
-	signal branch_mux_output : std_logic_vector(31 downto 0);
-	signal jumpreg_mux_output : std_logic_vector(31 downto 0);
-	signal jump_address : std_logic_vector(31 downto 0);
 	
-	-- data signals
+	-- multiplexor outputs (top right of datapath)
+	signal branch_mux_output : std_logic_vector(31 downto 0);
+	signal jump_mux_output : std_logic_vector(31 downto 0);
+	signal jumpreg_mux_output : std_logic_vector(31 downto 0);
+	
+	-- internal data signals
+	signal branch_adder_result : std_logic_vector(31 downto 0);
+	signal jump_address : std_logic_vector(31 downto 0);
 	signal immediate_extended : std_logic_vector(31 downto 0);
 	signal immediate_appended : std_logic_vector(31 downto 0);
 	signal shamt_extended : std_logic_vector(31 downto 0);
 	
 begin
-	----- create entities -----
-	alu_unit: entity work.alu port map(
-		aluop => aluop,
-		funccode => funct,
-		a => alu_in_a,
-		b => alu_in_b,
-		result => alu_result,
-		overflow => open,
-		zeroflag => zeroflag,
-		carryout => open
-	);
-	
-	register_unit: entity work.register_file port map(
-		clock => clk,
-		write_enable => regwrite,
-		read_reg1 => read_reg1,
-		read_reg2 => read_reg2,
-		write_reg => write_reg,
-		write_data => write_data,
-		read_data1 => read_data1,
-		read_data2 => read_data2
-	);
-	
+	----- MAIN COMPONENTS -----
+	-- control, instruction memory, registers, ALU, and data memory
 	control_unit: entity work.control port map(--
 		opcode => opcode,
 		funct => funct,
@@ -115,7 +94,47 @@ begin
 		regwrite => regwrite,
 		jumpreg => jumpreg
 	);
-
+	
+	instruction_memory: entity work.memory_unit port map(
+		clk => clk,
+		address => pc(5 downto 0), -- take 6 lsbs because we only have 64 memory words
+		data => x"00000000",
+		write_enable => '0',
+		output => instruction
+	);
+	
+	register_unit: entity work.register_file port map(
+		clock => clk,
+		write_enable => regwrite,
+		read_reg1 => rs,
+		read_reg2 => rt,
+		write_reg => write_reg,
+		write_data => write_data,
+		read_data1 => read_data1,
+		read_data2 => read_data2
+	);
+	
+	alu_unit: entity work.alu port map(
+		aluop => aluop,
+		funccode => funct,
+		a => alu_in_a,
+		b => alu_in_b,
+		result => alu_result,
+		overflow => open,
+		zeroflag => zeroflag,
+		carryout => open
+	);
+	
+	data_memory: entity work.memory_unit port map(
+		clk => clk,
+		address => alu_result(5 downto 0), -- take 6 lsbs because we only have 64 memory words
+		data => read_data2,
+		write_enable => memwrite,
+		output => dm_read_data
+	);
+	----- END MAIN COMPONENTS -----
+	
+	----- ADDERS -----
 	adder_pc4: entity work.adder port map(--
 		cin => '0',
 		a => pc,
@@ -139,23 +158,9 @@ begin
 		z => branch_adder_result,
 		cout => open
 	);
+	----- END ADDERS -----
 	
-	instruction_memory: entity work.memory_unit port map(
-		clk => clk,
-		address => pc(5 downto 0), -- take 6 lsbs because we only have 64 memory words
-		data => x"00000000",
-		write_enable => '0',
-		output => instruction
-	);
-	
-	data_memory: entity work.memory_unit port map(
-		clk => clk,
-		address => alu_result(5 downto 0), -- take 6 lsbs because we only have 64 memory words
-		data => read_data2,
-		write_enable => memwrite,
-		output => dm_read_data
-	);
-	
+	----- 2 INPUT MULTIPLEXERS -----
 	mux2_branch: entity work.mux2 port map(
 		sel => branch_sel,
 		in0 => pcplus4,
@@ -190,7 +195,10 @@ begin
 		in1 => immediate_extended,
 		output => alu_in_b
 	);
+	----- END 2 INPUT MULTIPLEXERS -----
 	
+	----- 4 INPUT MULTIPLEXERS -----
+	-- select data to write to the register
 	mux4_regdata: entity work.mux4 port map(
 		sel => regdatasel,
 		in0 => alu_result,
@@ -200,32 +208,45 @@ begin
 		output => write_data
 	);
 	
+	-- select destination register to write data to
 	mux4_regdst: entity work.mux4_5bit port map(--
 		sel => regdst,
-		in0 => instruction(20 downto 16),
-		in1 => instruction(15 downto 11),
-		in2 => "11111",
+		in0 => rt,
+		in1 => rd,
+		in2 => "11111", -- 31
 		in3 => "XXXXX",
 		output => write_reg
 	);
+	----- END 4 INPUT MULTIPLEXERS -----
 	
+	----- SIGNAL ASSIGNMENTS -----
+	-- instruction signals
+	opcode <= instruction(31 downto 26);
+	rs <= instruction(25 downto 21);
+	rt <= instruction(20 downto 16);
+	rd <= instruction(15 downto 11);
+	shamt <= instruction(10 downto 6);
+	funct <= instruction(5 downto 0);
+	immediate <= instruction(15 downto 0);
+	address <= instruction(25 downto 0);
+	
+	-- resized signals to fit 32 bits
+	shamt_extended <= x"000000" & "000" & shamt;
+	immediate_appended <= immediate & x"0000";
 	sign_extend_immediate: entity work.sign_extend port map(--
 		input => immediate,
 		output => immediate_extended
 	);
 	
-	----- signal assignments -----
+	-- branch mux selector
 	branch_sel <= zeroflag and branch;
-	opcode <= instruction(31 downto 26);
-	read_reg1 <= instruction(25 downto 21);
-	read_reg2 <= instruction(20 downto 16);
-	immediate <= instruction(15 downto 0);
-	shamt <= instruction(10 downto 6);
-	funct <= instruction(5 downto 0);
-	jump_address <= pcplus4(31 downto 28) & instruction(25 downto 0) & "00";
-	shamt_extended <= x"000000" & "000" & shamt;
-	immediate_appended <= immediate & x"0000";
 	
+	-- jump address is an offset from the PC's next value
+	jump_address <= pcplus4(31 downto 28) & address & "00";
+	----- END SIGNAL ASSIGNMENTS -----
+	
+	-- update PC on rising edge of the clock with the
+	-- output of the series of branch/jump MUXes
 	process (clk)
 	begin
 		if rising_edge(clk) then
